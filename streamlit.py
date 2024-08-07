f7f5afc98bdc026fb704c41b8f55852838606609
Malak Wehbe <51320105+MalakW@users.noreply.github.com>
2024-08-07 16:19:02 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -31,9 +31,7 @@ st.markdown(
 )
 
 def get_device():
-    if torch.backends.mps.is_available():
-        return torch.device("mps")
-    elif torch.cuda.is_available():
+    if torch.cuda.is_available():
         return torch.device("cuda")
     else:
         return torch.device("cpu")
