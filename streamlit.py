c972a2ec749c0385e0223d60b0ddb5ced00dbe17
Malak Wehbe <51320105+MalakW@users.noreply.github.com>
2024-08-11 22:28:20 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -276,14 +276,15 @@ def tab1():
 
             if result:
                 st.session_state[f"{tab_name}_sentiment"] = result[0]["label"]
-                if st.session_state[f"{tab_name}_sentiment"] == "LABEL_1":
-                    st.session_state[f"{tab_name}_sentiment_message"] = (
-                        "This text has a positive sentiment."
-                    )
-                else:
-                    st.session_state[f"{tab_name}_sentiment_message"] = (
-                        "This text has a negative sentiment."
-                    )
+                st.session_state[f"{tab_name}_sentiment_message"] = result
+                # if st.session_state[f"{tab_name}_sentiment"] == "LABEL_1":
+                #     st.session_state[f"{tab_name}_sentiment_message"] = (
+                #         "This text has a positive sentiment."
+                #     )
+                # else:
+                #     st.session_state[f"{tab_name}_sentiment_message"] = (
+                #         "This text has a negative sentiment."
+                #     )
             else:
                 st.session_state[f"{tab_name}_sentiment_message"] = (
                     "Unable to determine sentiment. Please try again with a different text."
